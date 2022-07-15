a7f403ac14bb97d7fd088a41f423ec74748dfbd8
elizeubh2006 <elizeubh2006@yahoo.com.br>
2022-07-15 21:45:44 +0000

    erro socket import

--- 1st-release/server.js
+++ 1st-release/server.js
@@ -2,8 +2,11 @@ import express from 'express'
 import http from 'http'
 import createGame from './public/game.js'
 
+import {Socket} from 'socket.io'; 
+
 const app = express()
 const server = http.createServer(app)
+const sockets = new Socket(server)
 
 app.use(express.static('public'))
 
@@ -15,7 +18,14 @@ game.addPlayer({playerId: 'player3', playerX: 5, playerY:0})
 game.addFruit({fruitId: 'fruit1', fruitX: 3, fruitY:3})
 game.addFruit({fruitId: 'fruit2', fruitX: 6, fruitY:3})
 
+game.movePlayer({playerId: 'player1', keyPressed: 'ArrowRight'})
+
 console.log(game.state)
+
+sockets.on('connecition', (socket) => {
+    const playerId = socket.id
+    console.log( `> Player connected on Server with id ${playerId}` )
+})
 
 server.listen(3000, () => {
     console.log(`> server listening on port: 3000`)
